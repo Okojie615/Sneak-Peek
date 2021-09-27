1f2293d607105e3eb749fe233ea2b1d68d51eb1e
nathanroses <87315662+nathanroses@users.noreply.github.com>
2021-09-27 18:38:45 -0500

    new

--- assets/script/sneaks.js
+++ assets/script/sneaks.js
@@ -1,7 +1,7 @@
 const SneaksAPI = require('sneaks-api');
 const sneaks = new SneaksAPI();
 
-//getProducts(keyword, limit, callback) takes in a keyword and limit and returns a product array 
+//getProduct(keyword, limit, callback) takes in a keyword and limit and returns a product array 
 sneaks.getProducts("Yeezy Cinder", 10, function(err, products){
     console.log(products)
 })
