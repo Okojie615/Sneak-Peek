dba69a0fbe24bf50cf48d6b2c5fb18418d61d585
David Okojie <david.okojie@gmail.com>
2021-09-29 18:07:31 -0500

    updating searchfunc files, html css js api

---
+++ assets/js/result-script.js
@@ -0,0 +1,48 @@
+const userSearch = localStorage.getItem("results");
+const savedSearch = document.getElementById("user-search"); 
+
+
+// window.onload()
+
+
+var x = localStorage.getItem("results");
+var y = localStorage.getItem("results"[1]);
+var z = localStorage.getItem("results"[2]);
+
+console.log(x,y,z);
+
+//Google.com/size=[usersearch[0]]&brand=[usersearch[1]]
+
+
+// var savedSearch = document.getElementById("user-search");
+// savedSearch.value = userSearch.value;
+// savedSearch.elements[1].value = userSearch.elements[1].value;
+// savedSearch.elements[2].value = userSearch.elements[2].value;
+
+
+
+// const submitButtonEl = document.getElementById("submit");
+// const valueSizeEl = document.getElementById('size');
+// const valueBrandEl = document.getElementById('brand');
+// const valueGenderEl = document.getElementById('gender');
+
+
+// submitButtonEl.addEventListener("click", shoeSearch);
+
+
+// //Function to search based on user paramaters
+// function shoeSearch() {
+// //Variable to store size, brand, gender
+// var resultsValueEl = [];
+// //Fetching user size selection from search form
+// var sizeValue = document.getElementById("user-search").elements[0].value;
+// //Fetching user brand selection from search form
+// var brandValue = document.getElementById("user-search").elements[1].value;
+// //Fetching user gender selection from search form
+// var genderValue = document.getElementById("user-search").elements[2].value;
+// //Declaring resultsValueEl as user selected values
+// resultsValueEl.push(sizeValue, brandValue, genderValue);
+// //Pushing resultsValeEl to local storage
+// localStorage.setItem("results", resultsValueEl);
+// };
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,7 @@
-// const SneaksAPI = require('sneaks-api');
-// const sneaks = new SneaksAPI();
-
-const submitButtonEl = document.querySelector("#Submit");
+const submitButtonEl = document.getElementById("submit");
 const valueSizeEl = document.getElementById('size');
 const valueBrandEl = document.getElementById('brand');
 const valueGenderEl = document.getElementById('gender');
-const returnedSearchEl = document.getElementById('results-column');
-
 
 
 submitButtonEl.addEventListener("click", shoeSearch);
@@ -14,51 +9,33 @@ submitButtonEl.addEventListener("click", shoeSearch);
 
 //Function to search based on user paramaters
 function shoeSearch() {
-// localStorage.clear();
+//Variable to store size, brand, gender
+var resultsValueEl = [];
+//Fetching user size selection from search form
+var sizeValue = document.getElementById("user-search").elements[0].value;
+//Fetching user brand selection from search form
+var brandValue = document.getElementById("user-search").elements[1].value;
+//Fetching user gender selection from search form
+var genderValue = document.getElementById("user-search").elements[2].value;
+//Declaring resultsValueEl as user selected values
+resultsValueEl.push(sizeValue, brandValue, genderValue);
+//Pushing resultsValeEl to local storage
+localStorage.setItem("results", resultsValueEl);
 
-//Variables to store size, brand, gender
-var resultsValueEl = [x,y,z];
-
-//Create div to hold these values
-var userSelectionEl = document.createElement("div");
-userSelectionEl.classname = "hero";
-
-
-var sizeSelectedEl = document.createElement("p");
-var x = document.getElementById("user-search").elements[0].value;
-sizeSelectedEl.innerHTML = x;
-
-var brandSelectedEl = document.createElement("p");
-var y = document.getElementById("user-search").elements[1].value;
-brandSelectedEl.innerHTML = y;
-
-var genderSelectedEl = document.createElement("p");
-var z = document.getElementById("user-search").elements[2].value;
-genderSelectedEl.innerHTML = z;
-
-
-//Display search back to the user
-userSelectionEl.innerHTML = "Your search for size " + y + ", " + z + ", " + x + " brand shoes:";
-
-returnedSearchEl.appendChild(userSelectionEl);
-
-// console.log(userSelectionEl.textContent);
-// console.log(resultsValueEl[2]);
-// console.log(returnedSearchEl.textContent);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(resultsValueEl);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
