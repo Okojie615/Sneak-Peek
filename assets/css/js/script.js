c531ca972173d083e2dcc02aecb864e5ca3a1873
j23click <j23click@gmail.com>
2021-10-04 00:52:34 -0500

    youtube loop fix

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -4,35 +4,54 @@
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // 3. This function creates an <iframe> (and YouTube player)
+  
+var section = {
+  start: 3,
+  end: 93 
+};
+
+// 3. This function creates an <iframe> (and YouTube player)
   //    after the API code downloads.
   var player;
   function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '700',
-      width: '100%',
-      videoId: 'LYgcQcH6_TQ',
-      playerVars: {
-        'playlist': 'LYgcQcH6_TQ',
-        'autoplay': 1,
-        'controls': 0,
-        'loop': 1,
-        'start': 3,
-        'end': 9,
-        'mute': 1,      
-      },
-      events: {
-        'onReady': onPlayerReady,
-
+    player = new YT.Player(
+      'player',
+      {
+        height: '700',
+        width: '100%',
+        videoId: 'LYgcQcH6_TQ',
+        playerVars: {
+          'playlist': 'LYgcQcH6_TQ',
+          'autoplay': 1,
+          'controls': 0,
+          'loop': 1,
+          'start': 3,
+          'end': 93,
+          'mute': 1,   
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
       }
-    });
+    );
   }
-
-  // 4. The API will call this function when the video player is ready.
+  
   function onPlayerReady(event) {
-    event.target.playVideo();
+    player.seekTo(section.start);
+    player.playVideo();
   }
-
+  
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+      var duration = section.end - section.start;
+      setTimeout(restartVideoSection, duration * 1000);
+    }
+  }
+  
+  function restartVideoSection() {
+    player.seekTo(section.start);
+  }
 
 
  
